bbf0949a035273bfff0d27c2707e71405e117f26
Thibault Cheneviere <thibault.chene23@gmail.com>
2023-11-05 17:09:32 -0500

    [+] add a more efficient solution for problem 1535

--- rust/1535/src/lib.rs
+++ rust/1535/src/lib.rs
@@ -3,10 +3,37 @@ use std::collections::VecDeque;
 pub struct Solution;
 
 impl Solution {
-    pub fn get_winner(mut arr: Vec<i32>, k: i32) -> i32 {
+    pub fn get_winner(arr: Vec<i32>, k: i32) -> i32 {
         if k as usize >= arr.len() {
-            arr.sort();
-            return arr[arr.len() - 1];
+            return *arr.iter().max().unwrap();
+        }
+
+        if k == 1 {
+            return std::cmp::max(arr[0], arr[1])
+        }
+
+        let mut win = 0;
+        let mut winner = arr[0];
+
+        for i in &arr[1..] {
+            if winner > *i {
+                win += 1;
+            } else {
+                winner = *i;
+                win = 1;
+            }
+
+            if win == k {
+                break;
+            }
+        }
+
+        winner
+    }
+
+    pub fn get_winner_first(arr: Vec<i32>, k: i32) -> i32 {
+        if k as usize >= arr.len() {
+            return *arr.iter().max().unwrap();
         }
 
         let mut win = 0;
@@ -55,7 +82,9 @@ mod tests {
             ([2, 1, 3, 5, 4, 6, 7], 2, 5),
             ([3, 2, 1], 10, 3),
             ([1, 9, 8, 2, 3, 7, 6, 4, 5], 7, 9),
-            ([1, 11, 22, 33, 44, 55, 66, 77, 88, 99], 100000, 99)
+            ([1, 11, 22, 33, 44, 55, 66, 77, 88, 99], 100000, 99),
+            ([11, 1, 2, 3], 1, 11),
+            ([1, 11, 2, 3, 5], 1, 11)
         );
     }
 }

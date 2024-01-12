f29a37bd2a61654fb6645dd1d0cf2f629c58a7a4
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-01-12 09:24:02 -0500

    feat: add new daily problem

---
+++ rust/1704/README.md
@@ -0,0 +1,3 @@
+# 1704. Determine if String Halves Are Alike
+
+[link to problem](https://leetcode.com/problems/determine-if-string-halves-are-alike/description/)
---
+++ rust/1704/src/lib.rs
@@ -0,0 +1,38 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn halves_are_alike(s: String) -> bool {
+        let filter = |c| match c {
+            'a' | 'A' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U' => true,
+            _ => false,
+        };
+
+        s.chars().take(s.len() / 2).filter(|c| filter(*c)).count()
+            == s.chars().skip(s.len() / 2).filter(|c| filter(*c)).count()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert!(Solution::halves_are_alike("book".to_string()));
+    }
+
+    #[test]
+    fn case2() {
+        assert!(!Solution::halves_are_alike("textbook".to_string()));
+    }
+
+    #[test]
+    fn case3() {
+        assert!(!Solution::halves_are_alike("MerryChristmas".to_string()));
+    }
+
+    #[test]
+    fn case4() {
+        assert!(Solution::halves_are_alike("AbCdEfGh".to_string()));
+    }
+}
